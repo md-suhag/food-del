77fd482071c6d173f83648647d1ed995b95cbab8
md-suhag <abdussalamsuhag104@gmail.com>
2024-06-02 18:44:40 +0900

    add add and list done

---
+++ admin/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Orders from "./pages/Orders/Orders";
+import List from "./pages/List/List";
+import Add from "./pages/Add/Add";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const url = "http://localhost:4000";
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
