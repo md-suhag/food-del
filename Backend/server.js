34214a6f6df6afd8f1f1ec653b2cfc12e156be5c
md-suhag <abdussalamsuhag104@gmail.com>
2024-06-16 18:43:22 +0600

    cors modified

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const corsConfig = {
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
-app.options("", cors(corsConfig));
+app.options("*", cors(corsConfig));
 app.use(express.json());
 
 app.use(cors(corsConfig));
