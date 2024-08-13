7e1c9509dbf2823dd542d1064539bfb53d259cbe
md-suhag <abdussalamsuhag104@gmail.com>
2024-08-13 19:44:30 +0600

    skeleton added

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,30 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./FoodDisplay.css";
 
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
+import Skeleton from "./../Skeleton/Skeleton";
 
 function FoodDisplay({ category }) {
   const { food_list } = useContext(StoreContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (food_list.length > 0) {
+      setLoading(false);
+    }
+    console.log(food_list);
+  }, [food_list]);
 
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodItem
-                key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-              />
-            );
-          }
-        })}
+        {loading ? (
+          <Skeleton />
+        ) : (
+          food_list.map((item, index) => {
+            if (category === "All" || category === item.category) {
+              return (
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                />
+              );
+            }
+          })
+        )}
       </div>
     </div>
   );
---
+++ frontend/src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Skeleton.css";
+
+const Skeleton = () => {
+  return (
+    <div className="skeleton-container">
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+    </div>
+  );
+};
+
+export default Skeleton;
